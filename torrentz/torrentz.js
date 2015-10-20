bcdbd09fd34fc21942636414573d33280945d3f3
Pisek <pisekfm@o2.pl>
2015-10-20 12:08:49 +0200

    smaller bg

--- torrentz/torrentz.js
+++ torrentz/torrentz.js
@@ -22,7 +22,7 @@
 (function(plugin) {
     var PREFIX = plugin.getDescriptor().id;
     var LOGO = plugin.path + "logo.png";
-    var BACKGROUND = plugin.path + "views/img/background.png";
+    var BACKGROUND = plugin.path + "views/img/background.jpg";
     
     var blue = '6699CC', orange = 'FFA500', red = 'EE0000', green = '008B45', yellow = 'FFFF00';
 
